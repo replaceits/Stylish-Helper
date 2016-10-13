190fac31e20e88f1f40581a7629a0853851babab
replaceits <sharpcut2231@gmail.com>
2016-10-12 20:48:44 -0400

    Added verbose, quiet, and help arguments. Added proper output.

--- DumpStylish.sh
+++ DumpStylish.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-usage() { echo "Usage: $0 [-i InputStylish.sqlite] [-o OutputStylishDirectory]" 1>&2; exit 1; }
+usage() { 
+	echo "Usage: $0 [-v {0-3}] [-q] [-h] -i InputStylish.sqlite -o OutputDirectory" 1>&2; 
+	exit 1; 
+}
 
-while getopts ":i:o:h:" a; do
+usageHelp() {
+	echo "Usage: $0 [-v {0-3}] [-q] [-h] -i InputStylish.sqlite -o OutputDirectory" 1>&2;
+	echo -e "\t-i\t[InputStylish.sqlite]\tSelect the stylish.sqlite file to dump\n" 1>&2;
+	echo -e "\t-o\t[OutputDirectory]\tSelect the output directory to dump the contents, if directory doesn't exist it will be made\n" 1>&2;	
+	echo -e "\t-v\t[verbose level 0-3]" 1>&2;
+	echo -e "\t\t0\t quiet (no) output" 1>&2;
+	echo -e "\t\t1\t normal level of output" 1>&2;
+	echo -e "\t\t2\t a bit more information" 1>&2;
+	echo -e "\t\t3\t a ton of information\n" 1>&2;
+	echo -e "\t-q\tQuiet (no) output (same as -v 0)\n" 1>&2;
+	echo -e "\t-h\tDisplay help" 1>&2;
+	exit 1;
+}
+
+v="1"
+
+while getopts ":i:o:h :v: :q " a; do
 	case "${a}" in
 		i)
 			i=${OPTARG}
@@ -10,8 +29,14 @@ while getopts ":i:o:h:" a; do
 		o)
 			o=${OPTARG}
 			;;
+		v)
+			v=${OPTARG}
+			;;
+		q)
+			v="0"
+			;;
 		h)
-			usage
+			usageHelp
 			;;
 		*)
 			usage
@@ -20,7 +45,7 @@ while getopts ":i:o:h:" a; do
 done
 shift $((OPTIND-1))
 
-if [ -z "${i}" ] || [ -z "${o}" ] || [ -e "${o}" ] && [ -f "${o}" ] || [ ! -f "${i}" ] || !(file -ib ${i} | grep -Fxq "application/x-sqlite3; charset=binary") ; then
+if [ -z "${i}" ] || [ -z "${o}" ] || [ -e "${o}" ] && [ -f "${o}" ] || [ ! -f "${i}" ] || !(file -ib ${i} | grep -Fxq "application/x-sqlite3; charset=binary") ||  ! [[ "$v" =~ ^-?[0-9]*$ ]] || [ "$v" -lt 0 ] || [ "$v" -gt 3 ] ; then
 	usage
 fi
 
@@ -72,5 +97,32 @@ sqlite3 "${i}" "SELECT id FROM styles" | while read id; do
 
 	style_meta_value="$(sqlite3 "${i}" "SELECT value FROM style_meta WHERE style_id=${id}")"
 	echo "${style_meta_value}" >> "${o}/${name}/.style_meta"
+
+	if [ "$v" -ge 1 ] ; then
+		echo -n "Exported $name"
+		if [ "$v" -ge 2 ] ; then
+			echo -n " to ${o}/${name}"
+			if [ "$v" -ge 3 ] ; then
+				echo -n -e "\n\t${o}/${name}/.styles"
+				echo -n -e "\n\t\t${id}"
+				echo -n -e "\n\t\t${url}"
+				echo -n -e "\n\t\t${updateUrl}"
+				echo -n -e "\n\t\t${md5Url}"
+				echo -n -e "\n\t\t${name}"
+				echo -n -e "\n\t\t${enabled}"
+				echo -n -e "\n\t\t${originalCode}"
+				echo -n -e "\n\t\t${idUrl}"
+				echo -n -e "\n\t\t${applyBackgroundUpdates}"
+				echo -n -e "\n\t\t${originalMd5}"
+				echo -n -e "\n\t${o}/${name}/${name}.css"
+				echo -n -e "\n\t${o}/${name}/.style_meta"
+				echo -n -e "\n\t\t${style_meta_id}"
+				echo -n -e "\n\t\t${id}"
+				echo -n -e "\n\t\t${style_meta_name}"
+				echo -n -e "\n\t\t${style_meta_value}"
+			fi
+		fi
+		echo ""
+	fi
 done
 
